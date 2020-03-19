0db07c8215dc5fe05bc7ec897666a75c4ee720bd
David Knipper <dknipper@davidknipper.com>
2020-03-19 09:32:18 -0600

    product desc

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators/map';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number): Observable<any> {
+    return this._http.get(this._albumUrl).pipe(
+      map(response => response.json())
+      );
+  }
+}
