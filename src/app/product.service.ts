0a3344c17503064268fbf3a414cb9f41342048cb
David Knipper <dknipper@davidknipper.com>
2020-03-19 10:02:35 -0600

    album interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
@@ -10,7 +12,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album> response.json());
   }
 }
